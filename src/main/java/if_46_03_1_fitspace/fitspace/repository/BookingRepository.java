523c349c51110447721f64687dbcbfac30fe667f
Althaf Rizqullah <althaf8747@gmail.com>
2025-01-04 10:19:02 +0700

    BookingEntity

---
+++ src/main/java/if_46_03_1_fitspace/fitspace/repository/BookingRepository.java
@@ -0,0 +1,14 @@
+package if_46_03_1_fitspace.fitspace.repository;
+
+import fitspace.fitspace_sports_venue_booking_website_backend.entity.Booking;
+import fitspace.fitspace_sports_venue_booking_website_backend.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface BookingRepository extends JpaRepository<Booking, Long> {
+    List<Booking> findAllByCustomer(User customer);
+
+    Optional<Booking> findByCustomerAndId(User customer,Long id);
+}
