983f7f41a0ad330547d56861da6c04f2bb599f4c
Fatah Fadhlur Rohman FN <fatahrohman152@gmail.com>
2025-01-08 21:58:55 +0700

    Review

---
+++ src/main/java/if_46_03_1_fitspace/fitspace/controller/ReviewController.java
@@ -0,0 +1,95 @@
+package if_46_03_1_fitspace.fitspace.controller;
+
+import fitspace.fitspace_sports_venue_booking_website_backend.dto.WebResponse;
+import fitspace.fitspace_sports_venue_booking_website_backend.dto.bookings.BookingAddRequest;
+import fitspace.fitspace_sports_venue_booking_website_backend.dto.review.ReviewAddRequest;
+import fitspace.fitspace_sports_venue_booking_website_backend.dto.review.ReviewDataResponse;
+import fitspace.fitspace_sports_venue_booking_website_backend.dto.review.ReviewUpdateRequest;
+import fitspace.fitspace_sports_venue_booking_website_backend.entity.User;
+import fitspace.fitspace_sports_venue_booking_website_backend.service.ReviewService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Slf4j
+@RestController
+public class ReviewController {
+
+    @Autowired
+    private ReviewService reviewService;
+
+    @PostMapping(
+            path = "/api/fields/{fieldId}/reviews",
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public WebResponse<ReviewDataResponse> create(@PathVariable("fieldId") Integer fieldId , User user, @RequestBody ReviewAddRequest request){
+        ReviewDataResponse reviewDataResponse = reviewService.create(fieldId,user,request);
+
+        return WebResponse.<ReviewDataResponse>builder()
+                .code(HttpStatus.OK.value())
+                .status(HttpStatus.OK.getReasonPhrase())
+                .data(reviewDataResponse)
+                .build();
+    }
+
+    @DeleteMapping(
+            path = "/api/reviews/{reviewId}",
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public WebResponse<String> delete(@PathVariable("reviewId") Long reviewId){
+        reviewService.delete(reviewId);
+        return WebResponse.<String>builder()
+                .code(HttpStatus.OK.value())
+                .status(HttpStatus.OK.getReasonPhrase())
+                .data("Review Deleted Successfully")
+                .build();
+    }
+
+    @GetMapping(
+            path = "/api/{fieldId}/reviews",
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public WebResponse<List<ReviewDataResponse>> getAllReviews(@PathVariable("fieldId") Integer fieldId){
+        List<ReviewDataResponse> reviewDataResponses = reviewService.getAllReviews(fieldId);
+        return WebResponse.<List<ReviewDataResponse>>builder()
+                .code(HttpStatus.OK.value())
+                .status(HttpStatus.OK.getReasonPhrase())
+                .data(reviewDataResponses)
+                .build();
+    }
+
+    @GetMapping(
+            path = "/api/reviews",
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public WebResponse<List<ReviewDataResponse>> getAlls(){
+        List<ReviewDataResponse> reviewDataResponses = reviewService.getAlls();
+        return WebResponse.<List<ReviewDataResponse>>builder()
+                .code(HttpStatus.OK.value())
+                .status(HttpStatus.OK.getReasonPhrase())
+                .data(reviewDataResponses)
+                .build();
+    }
+
+    @PatchMapping(
+            path = "/api/reviews/{reviewId}",
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public WebResponse<ReviewDataResponse> update(@PathVariable("reviewId") Long reviewId, @RequestBody ReviewUpdateRequest request){
+        ReviewDataResponse review = reviewService.update(reviewId,request);
+        return WebResponse.<ReviewDataResponse>builder()
+                .code(HttpStatus.OK.value())
+                .status(HttpStatus.OK.getReasonPhrase())
+                .data(review)
+                .build();
+    }
+
+
+}
---
+++ src/main/java/if_46_03_1_fitspace/fitspace/service/ReviewService.java
@@ -0,0 +1,106 @@
+package if_46_03_1_fitspace.fitspace.service;
+
+import fitspace.fitspace_sports_venue_booking_website_backend.dto.review.ReviewAddRequest;
+import fitspace.fitspace_sports_venue_booking_website_backend.dto.review.ReviewDataResponse;
+import fitspace.fitspace_sports_venue_booking_website_backend.dto.review.ReviewUpdateRequest;
+import fitspace.fitspace_sports_venue_booking_website_backend.entity.Field;
+import fitspace.fitspace_sports_venue_booking_website_backend.entity.Review;
+import fitspace.fitspace_sports_venue_booking_website_backend.entity.User;
+import fitspace.fitspace_sports_venue_booking_website_backend.helper.EntityToDtoMapper;
+import fitspace.fitspace_sports_venue_booking_website_backend.repository.FieldRepository;
+import fitspace.fitspace_sports_venue_booking_website_backend.repository.ReviewRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ReviewService {
+
+    @Autowired
+    private ValidationService validationService;
+
+    @Autowired
+    private ReviewRepository reviewRepository;
+
+    @Autowired
+    private FieldRepository fieldRepository;
+
+
+    @Transactional
+    public ReviewDataResponse create(Integer fieldId, User user, ReviewAddRequest request) {
+        validationService.validate(request);
+
+        Review review = new Review();
+        Field field = fieldRepository.findById(fieldId)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Field not found"));;
+        review.setComment(request.getComment());
+        review.setField(field);
+        review.setUser(user);
+        review.setRating(request.getRating());
+
+        try {
+            reviewRepository.save(review);
+
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Error saving review");
+        }
+
+        return EntityToDtoMapper.toReviewDataResponse(review);
+    }
+
+    @Transactional(readOnly = true)
+    public List<ReviewDataResponse> getAllReviews(Integer fieldId) {
+        List<Review> reviews = reviewRepository.findAllByField_Id(fieldId);
+
+        if(reviews == null || reviews.isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Review not found");
+        }
+
+        return reviews.stream()
+                .map(EntityToDtoMapper::toReviewDataResponse)
+                .toList();
+    }
+
+    @Transactional
+    public void delete(Long reviewId) {
+        Review review = reviewRepository.findById(reviewId)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Review not found"));
+
+        reviewRepository.delete(review);
+    }
+
+    @Transactional(readOnly = true)
+    public List<ReviewDataResponse> getAlls() {
+        List<Review> reviews = reviewRepository.findAll();
+
+        return reviews.stream()
+                .map(EntityToDtoMapper::toReviewDataResponse)
+                .toList();
+    }
+
+    @Transactional
+    public ReviewDataResponse update(long review_id, ReviewUpdateRequest request) {
+        Review review = reviewRepository.findById(review_id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Review not found"));
+
+        if(request.getComment() != null) {
+            review.setComment(request.getComment());
+        }
+
+        if(request.getRating() != null) {
+            review.setRating(request.getRating());
+        }
+
+        reviewRepository.save(review);
+
+        return EntityToDtoMapper.toReviewDataResponse(review);
+    }
+
+}
