7ef6be5f689bb6f452af2b81fc10ae4842134f08
Jihanalyh <jihanauliyah1@gmail.com>
2025-01-08 17:43:14 +0700

    repository venue

---
+++ src/main/java/if_46_03_1_fitspace/fitspace/repository/VenueRepository.java
@@ -0,0 +1,19 @@
+package if_46_03_1_fitspace.fitspace.repository;
+
+import fitspace.fitspace_sports_venue_booking_website_backend.entity.User;
+import fitspace.fitspace_sports_venue_booking_website_backend.entity.Venue;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface VenueRepository extends JpaRepository<Venue, Long> {
+    Optional<Venue> findFirstByOwnerAndId(User owner, Long id);
+    List<Venue> findAllByOwner(User owner);
+
+    @Query("SELECT v FROM Venue v WHERE v.fields IS NOT EMPTY")
+    List<Venue> findAllWithFields();
+}
